20cf8558b2b5485b5027819deac2d235c6648d27
Yang Yang <kiooss@gmail.com>
2016-01-15 16:05:12 +0900

    Change file name.

---
+++ zsh/functions.zsh
@@ -0,0 +1,16 @@
+#// vim: set ft=zsh:
+
+function make-writeable {
+    echo $*
+    APACHEUSER=`ps aux | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data' | grep -v root | head -1 | cut -d\  -f1`
+    sudo setfacl -R -m u:"$APACHEUSER":rwX -m u:`whoami`:rwX $*
+    sudo setfacl -dR -m u:"$APACHEUSER":rwX -m u:`whoami`:rwX $*
+}
+
+function restart-ssh-agent {
+    killall ssh-agent; eval `ssh-agent`
+}
+
+function show_user_list {
+    cat /etc/passwd | cut -d ":" -f1
+}

eaf5395df2f29b5edf9c7bbb2adfe22dd0d5ffee
Yang Yang <kiooss@gmail.com>
2016-01-16 18:06:34 +0900

    Update zsh config. add openssl helper.

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -35,3 +35,54 @@ function update_dotfiles() {
         echo "Failed!"
     fi
 }
+
+# Generate an SSL key and a signing request or self-signed certificate
+function sslcert() {
+    cn=$1
+
+    # The prefix for the certificate's subject, eg
+    # SUBJ="/C=GB/ST=Edinburgh/L=Edinburgh/O=Widget Co"
+    SUBJ="/C=JP/ST=Tokyo/L=Tokyo/O=Webimpact Co"
+
+    if [ -z "$cn" -o "$cn" = "-h" ]; then
+        echo "usage: $0 <common name> [csr|crt]" >&2
+        echo "  csr - generate a certificate signing request (default)" >&2
+        echo "  crt - generate a self-signed certificate" >&2
+        return 1
+    fi
+
+    type=${2:-csr}
+
+    name=$(echo $cn | sed -e 's/^\*\./star./')
+    if [ -r $name ]; then
+        echo "$0: $name already exists"
+        return 1
+    fi
+    mkdir $name
+    if [ $? -ne 0 ]; then
+        echo "$0: can't mkdir $name" >&2
+        return 1
+    fi
+    cd $name
+    openssl genrsa -out ${name}.key 4096
+    case $type in
+    csr)
+        openssl req -new -key ${name}.key -out ${name}.csr -sha256 -subj "${SUBJ}/CN={cn}"
+        ;;
+    crt)
+        openssl req -new -x509 -days 3650 -key ${name}.key -out ${name}.crt -sha256 -subj "${SUBJ}/CN=${cn}"
+        ;;
+    esac
+    cd ..
+}
+
+function apache_ssl_config_template() {
+cat << EOF
+SSLEngine on
+SSLCertificateFile /usr/local/apache/conf/ssl.crt/server.crt
+SSLCertificateKeyFile /usr/local/apache/conf/ssl.key/server.key
+SetEnvIf User-Agent ".*MSIE.*" nokeepalive ssl-unclean-shutdown
+CustomLog logs/ssl_request_log \
+   "%t %h %{SSL_PROTOCOL}x %{SSL_CIPHER}x \"%r\" %b"
+EOF
+}

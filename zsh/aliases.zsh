ed864303f4f0ae97b37c8805da8862bcfa665f0e
Yang Yang <kiooss@gmail.com>
2016-01-21 16:32:34 +0900

    Change zsh config.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -79,3 +79,7 @@ alias chromekill="ps ux | grep '[C]hrome Helper --type=renderer' | grep -v exten
 
 alias chrome="/Applications/Google\\ \\Chrome.app/Contents/MacOS/Google\\ \\Chrome"
 alias canary="/Applications/Google\\ Chrome\\ Canary.app/Contents/MacOS/Google\\ Chrome\\ Canary"
+
+# android development
+alias adbstart="adb shell am start -n"
+alias gradlew="./gradlew"

bbf42ac925094fa084f019ef39176eb1c2a59111
Yang Yang <kiooss@gmail.com>
2016-09-29 12:35:00 +0900

    Update zsh config.

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -2,7 +2,7 @@
 # zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
-zstyle ':completion:*' insert-tab pending
+# zstyle ':completion:*' insert-tab pending
 
 # default to file completion
-zstyle ':completion:*' completer _expand _complete _files _correct _approximate
+# zstyle ':completion:*' completer _expand _complete _files _correct _approximate

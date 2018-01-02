185f96eefb8851083553a6526a529801edc69d2e
Yang Yang <kiooss@gmail.com>
2018-01-02 13:21:11 +0900

    Update config.

--- zsh/plugins/fix-ssh-agent.plugin.zsh
+++ zsh/plugins/fix-ssh-agent.plugin.zsh
@@ -1,8 +1,3 @@
 fixagent() {
-    for key in SSH_AUTH_SOCK SSH_CONNECTION SSH_CLIENT; do
-        if (tmux show-environment | grep "^${key}" > /dev/null); then
-            value=`tmux show-environment | grep "^${key}" | sed -e "s/^[A-Z_]*=//"`
-            export ${key}="${value}"
-        fi
-    done
+    eval $(tmux show-env -s | grep '^SSH_')
 }

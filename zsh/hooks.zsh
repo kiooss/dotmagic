36d3e801f71a7f38c859c612de59f8a1dd618593
Yang Yang <kiooss@gmail.com>
2018-01-10 18:17:23 +0900

    Update zsh config.

---
+++ zsh/hooks.zsh
@@ -0,0 +1,15 @@
+#
+# Hooks
+#
+autoload -U add-zsh-hook
+
+function auto-ls-after-cd() {
+  emulate -L zsh
+  # Only in response to a user-initiated `cd`, not indirectly (eg. via another
+  # function).
+  if [ "$ZSH_EVAL_CONTEXT" = "toplevel:shfunc" ]; then
+    ls -AlhF --color
+  fi
+}
+
+add-zsh-hook chpwd auto-ls-after-cd

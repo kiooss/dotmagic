7a065566b6641ce7a53fbfe73365292df2afa33b
Yang Yang <kiooss@gmail.com>
2018-01-10 14:59:29 +0900

    Seperate aliases.

--- zsh/plugins/aliases.zsh
+++ zsh/plugins/aliases.zsh
@@ -1,3 +1,13 @@
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+fi
+
+# enable color support of ls and also add handy aliases (OSX)
+if [ -x /usr/local/bin/gdircolors ]; then
+    test -r ~/.dircolors && eval "$(gdircolors -b ~/.dircolors)" || eval "$(gdircolors -b)"
+    # alias ls='gls'
+fi
 # reload zsh config
 alias reload!='source ~/.zshrc'
 
@@ -9,6 +19,7 @@ else # OS X `ls`
 fi
 
 alias vi="vim"
+# alias view="vim -R"
 
 alias tf="tail -f"
 
@@ -18,10 +29,11 @@ alias ...='cd ../..'
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
 
-alias l="ls -lah ${colorflag}"
+alias l="ls -CF ${colorflag}"
 alias la="ls -AF ${colorflag}"
-alias ll="ls -lFh ${colorflag}"
+alias ll="ls -lahF ${colorflag}"
 alias lld="ls -l | grep ^d"
+
 alias rmf="rm -rf"
 
 # Helpers
@@ -30,26 +42,14 @@ alias df='df -h' # disk free, in Gigabytes, not bytes
 alias du='du -h -c' # calculate disk usage for a folder
 alias groot='cd $(git rev-parse --show-toplevel 2> /dev/null || echo -n ".")'
 
-# Applications
-alias ios='open -a /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/Applications/iPhone\ Simulator.app'
-
 # IP addresses
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias localip="ipconfig getifaddr en1"
 alias ips="ifconfig -a | perl -nle'/(\d+\.\d+\.\d+\.\d+)/ && print $1'"
 
-# Flush Directory Service cache
-alias flush="dscacheutil -flushcache"
-
 # View HTTP traffic
 alias sniff="sudo ngrep -d 'en0' -t '^(GET|POST) ' 'tcp and port 80'"
 alias httpdump="sudo tcpdump -i en0 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
-
-# Trim new lines and copy to clipboard
-alias trimcopy="tr -d '\n' | pbcopy"
-
-# Recursively delete `.DS_Store` files
-alias cleanup="find . -name '*.DS_Store' -type f -ls -delete"
 
 # File size
 alias fs="stat -f \"%z bytes\""
@@ -57,53 +57,13 @@ alias fs="stat -f \"%z bytes\""
 # ROT13-encode text. Works for decoding, too! ;)
 alias rot13='tr a-zA-Z n-za-mN-ZA-M'
 
-# Empty the Trash on all mounted volumes and the main HDD
-alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; rm -rfv ~/.Trash"
-
-# Hide/show all desktop icons (useful when presenting)
-alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
-alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
-
 # One of @janmoesen’s ProTip™s
 for method in GET HEAD POST PUT DELETE TRACE OPTIONS; do
     alias "$method"="lwp-request -m '$method'"
 done
 
-# Stuff I never really use but cannot delete either because of http://xkcd.com/530/
-alias stfu="osascript -e 'set volume output muted true'"
-alias pumpitup="osascript -e 'set volume 10'"
-
-# Kill all the tabs in Chrome to free up memory
-# [C] explained: http://www.commandlinefu.com/commands/view/402/exclude-grep-from-your-grepped-output-of-ps-alias-included-in-description
-alias chromekill="ps ux | grep '[C]hrome Helper --type=renderer' | grep -v extension-process | tr -s ' ' | cut -d ' ' -f2 | xargs kill"
-
-alias chrome="/Applications/Google\\ \\Chrome.app/Contents/MacOS/Google\\ \\Chrome"
-alias canary="/Applications/Google\\ Chrome\\ Canary.app/Contents/MacOS/Google\\ Chrome\\ Canary"
-
-# android development
-alias adbstart="adb shell am start -n"
-alias gradlew="./gradlew"
-alias adb_focus="adb shell dumpsys window windows | grep -E 'mCurrentFocus|mFocusedApp'"
-
 # tmuxinator
 alias mux="tmuxinator"
-
-# enable color support of ls and also add handy aliases
-if [ -x /usr/bin/dircolors ]; then
-    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-    alias ls='ls -hF --color=auto'
-fi
-
-# enable color support of ls and also add handy aliases (OSX)
-if [ -x /usr/local/bin/gdircolors ]; then
-    test -r ~/.dircolors && eval "$(gdircolors -b ~/.dircolors)" || eval "$(gdircolors -b)"
-    alias ls='gls -hF --color=auto'
-fi
-
-# some more ls aliases
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
 
 alias stop_meteor="kill -9 `ps ax | grep node | grep meteor | awk '{print $1}'`"
 alias eucssh='cocot -t UTF-8 -p EUC-JP ssh'
@@ -116,5 +76,3 @@ alias bb="bundle open"
 alias sqlplus="rlwrap sqlplus"
 alias use_php4="sudo a2dismod php5 && sudo service apache2 restart"
 alias use_php5="sudo a2enmod php5 && sudo service apache2 restart"
-
-alias view="vim -R"

53ac6b7d22cc66588126d7976f240e5405110ef1
Yang Yang <kiooss@gmail.com>
2018-01-15 13:42:20 +0900

    Update zsh config.

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -29,12 +29,12 @@ setopt COMPLETE_ALIASES
 setopt GLOBDOTS
 
 # make terminal command navigation sane again
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[3~' delete-char
-bindkey '^?' backward-delete-char
+# bindkey '^[^[[D' backward-word
+# bindkey '^[^[[C' forward-word
+# bindkey '^[[5D' beginning-of-line
+# bindkey '^[[5C' end-of-line
+# bindkey '^[[3~' delete-char
+# bindkey '^?' backward-delete-char
 
 fpath=($DOTFILES/zsh/functions $fpath)
 autoload -U $DOTFILES/zsh/functions/*(:t)

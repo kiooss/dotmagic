665158a3c666377ca174611d7820f7a1b0462001
Yang Yang <kiooss@gmail.com>
2018-01-19 10:23:46 +0900

    Update.

--- bin/wan_ip.sh
+++ bin/wan_ip.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 # Prints the WAN IP address. The result is cached and updated according to $update_period.
 
 wan_ip() {
@@ -9,12 +9,12 @@ wan_ip() {
 		if [[ "$SHELL_PLATFORM" == 'osx' || "$SHELL_PLATFORM" == 'bsd' ]]; then
 			stat >/dev/null 2>&1 && is_gnu_stat=false || is_gnu_stat=true
 			if [ "$is_gnu_stat" == "true" ];then
-				last_update=$(stat -c "%Y" ${tmp_file})
+				last_update=$(stat -c "%Y" "${tmp_file}")
 			else
-				last_update=$(stat -f "%m" ${tmp_file})
+				last_update=$(stat -f "%m" "${tmp_file}")
 			fi
 		elif [[ "$SHELL_PLATFORM" == 'linux' || -z $is_gnu_stat ]]; then
-			last_update=$(stat -c "%Y" ${tmp_file})
+			last_update=$(stat -c "%Y" "${tmp_file}")
 		fi
 
 		time_now=$(date +%s)
@@ -22,7 +22,7 @@ wan_ip() {
 		up_to_date=$(echo "(${time_now}-${last_update}) < ${update_period}" | bc)
 
 		if [ "$up_to_date" -eq 1 ]; then
-			wan_ip=$(cat ${tmp_file})
+			wan_ip=$(cat "${tmp_file}")
 		fi
 	fi
 
@@ -30,7 +30,7 @@ wan_ip() {
 		wan_ip=$(curl --max-time 2 -s http://whatismyip.akamai.com/)
 
 		if [ "$?" -eq "0" ]; then
-			echo "${wan_ip}" > $tmp_file
+			echo "${wan_ip}" > "$tmp_file"
 		elif [ -f "${tmp_file}" ]; then
 			wan_ip=$(cat "$tmp_file")
 		fi

0efeeacb6fce08a22838a48979c90520d29d700a
Yang Yang <kiooss@gmail.com>
2018-01-19 17:34:12 +0900

    Update.

--- zsh/lib/post_oh_my_zsh.zsh
+++ zsh/lib/post_oh_my_zsh.zsh
@@ -11,18 +11,18 @@ ZSH_HIGHLIGHT_STYLES[hashed-command]='fg=green,bold'
 
 
 rule () {
-	print -Pn '%F{blue}'
-	local columns=$(tput cols)
-	for ((i=1; i<=columns; i++)); do
-	   printf "\u2588"
-	done
-	print -P '%f'
+  print -Pn '%F{blue}'
+  local columns=$(tput cols)
+  for ((i=1; i<=columns; i++)); do
+    printf "\u2588"
+  done
+  print -P '%f'
 }
 
 function _my_clear() {
-	echo
-	rule
-	zle clear-screen
+  echo
+  rule
+  zle clear-screen
 }
 zle -N _my_clear
 bindkey '^l' _my_clear
@@ -30,15 +30,15 @@ bindkey '^l' _my_clear
 
 # Ctrl-O opens zsh at the current location, and on exit, cd into ranger's last location.
 ranger-cd() {
-	tempfile=$(mktemp)
-	ranger --choosedir="$tempfile" "${@:-$(pwd)}" < $TTY
-	test -f "$tempfile" &&
-	if [ "$(cat -- "$tempfile")" != "$(echo -n `pwd`)" ]; then
-	cd -- "$(cat "$tempfile")"
-	fi
-	rm -f -- "$tempfile"
-	# hacky way of transferring over previous command and updating the screen
-	VISUAL=true zle edit-command-line
+tempfile=$(mktemp)
+ranger --choosedir="$tempfile" "${@:-$(pwd)}" < $TTY
+test -f "$tempfile" &&
+  if [ "$(cat -- "$tempfile")" != "$(echo -n `pwd`)" ]; then
+    cd -- "$(cat "$tempfile")"
+  fi
+  rm -f -- "$tempfile"
+  # hacky way of transferring over previous command and updating the screen
+  VISUAL=true zle edit-command-line
 }
 zle -N ranger-cd
 bindkey '^o' ranger-cd

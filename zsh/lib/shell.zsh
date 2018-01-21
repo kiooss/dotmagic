b5e5707d9ee4e4930f21fdc0d21be3640c02fb3f
Yang Yang <kiooss@gmail.com>
2018-01-21 09:51:36 +0900

    CS.

--- zsh/lib/shell.zsh
+++ zsh/lib/shell.zsh
@@ -1,12 +1,11 @@
 # Shell Configuration
-# vi: sw=8 ts=8 noet
 
 ostype() { echo $OSTYPE | tr '[A-Z]' '[a-z]'; }
 
 export SHELL_PLATFORM='unknown'
 
 case "$(ostype)" in
-	*'linux'*	) SHELL_PLATFORM='linux'	;;
-	*'darwin'*	) SHELL_PLATFORM='osx'		;;
-	*'bsd'*		) SHELL_PLATFORM='bsd'		;;
+  *'linux'*)  SHELL_PLATFORM='linux';;
+  *'darwin'*) SHELL_PLATFORM='osx';;
+  *'bsd'*)    SHELL_PLATFORM='bsd';;
 esac

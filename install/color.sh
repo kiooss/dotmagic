3c19c7ddba3061420ba029927352326fb9b00bfb
Yang Yang <kiooss@gmail.com>
2018-01-23 19:29:04 +0900

    Improve color support.

--- install/color.sh
+++ install/color.sh
@@ -4,17 +4,25 @@ if which tput >/dev/null 2>&1; then
   ncolors=$(tput colors)
 fi
 if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+  BLACK="$(tput setaf 0)"
   RED="$(tput setaf 1)"
   GREEN="$(tput setaf 2)"
   YELLOW="$(tput setaf 3)"
   BLUE="$(tput setaf 4)"
+  MAGENTA="$(tput setaf 5)"
+  CYAN="$(tput setaf 6)"
+  WHITE="$(tput setaf 7)"
   BOLD="$(tput bold)"
   NORMAL="$(tput sgr0)"
 else
+  BLACK=""
   RED=""
   GREEN=""
   YELLOW=""
   BLUE=""
+  MAGENTA=""
+  CYAN=""
+  WHITE=""
   BOLD=""
   NORMAL=""
 fi

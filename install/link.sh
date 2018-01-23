815d986312e2ec446e1aa0452efa34f093e5a3c4
Yang Yang <kiooss@gmail.com>
2018-01-23 11:07:43 +0900

    Move symlinks to link dir.

--- install/link.sh
+++ install/link.sh
@@ -2,16 +2,23 @@
 
 DOTFILES=$HOME/.dotfiles
 
+force="$1"
+
 printf "\n${YELLOW}Creating symlinks\n"
-printf "==============================${NORMAL}\n"
-linkables=$( find -H "$DOTFILES" -maxdepth 3 -name '*.symlink' )
+printf "===========================================================${NORMAL}\n"
+linkables=$( find -H "$DOTFILES/link" -maxdepth 3 -name '*.symlink' )
 for file in $linkables ; do
     target="$HOME/.$( basename $file ".symlink" )"
-    if [ -e $target ]; then
-        echo "~${target#$HOME} already exists... Skipping."
+    if [ -e "$target" ]; then
+        if [ "$force" = "force" ]; then
+            echo "~${target#$HOME} already exists... Creating it forcelly."
+            ln -sf "$file" "$target"
+        else
+            echo "~${target#$HOME} already exists... Skipping."
+        fi
     else
         echo "Creating symlink for $file"
-        ln -s $file $target
+        ln -sf "$file" "$target"
     fi
 done
 

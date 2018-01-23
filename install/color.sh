0ba11950f99c0eaa6c48b9af0acad7d7094ffae0
Yang Yang <kiooss@gmail.com>
2018-01-23 11:54:56 +0900

    Improve link.sh

---
+++ install/color.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if which tput >/dev/null 2>&1; then
+  ncolors=$(tput colors)
+fi
+if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+  RED="$(tput setaf 1)"
+  GREEN="$(tput setaf 2)"
+  YELLOW="$(tput setaf 3)"
+  BLUE="$(tput setaf 4)"
+  BOLD="$(tput bold)"
+  NORMAL="$(tput sgr0)"
+else
+  RED=""
+  GREEN=""
+  YELLOW=""
+  BLUE=""
+  BOLD=""
+  NORMAL=""
+fi

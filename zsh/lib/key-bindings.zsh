55eb23e462f13101e3df47da4b9400d7c5885e26
Yang Yang <kiooss@gmail.com>
2018-02-02 15:24:31 +0900

    Add zsh key-bindings config.

---
+++ zsh/lib/key-bindings.zsh
@@ -0,0 +1,7 @@
+# use the vi navigation keys in menu completion
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
+
+bindkey ^Y accept-search

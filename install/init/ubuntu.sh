3a926b699a0166dce3bb4ea36d275b92177d15e1
Yang Yang <kiooss@gmail.com>
2018-02-05 17:21:02 +0900

    Neovim install for ubuntu.

--- install/init/ubuntu.sh
+++ install/init/ubuntu.sh
@@ -48,11 +48,34 @@ apt_packages+=(
 
 # apt_packages+=(vim)
 
+# https://github.com/neovim/neovim/wiki/Installing-Neovim
+add_ppa ppa:neovim-ppa/stable
+apt_packages+=(neovim)
+
 # https://github.com/rbenv/ruby-build/wiki
 apt_packages+=(
   autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev
   libncurses5-dev libffi-dev libgdbm3 libgdbm-dev zlib1g-dev
 )
+
+# Add APT sources.
+function __temp() { [[ ! -e /etc/apt/sources.list.d/$1.list ]]; }
+source_i=($(array_filter_i apt_source_files __temp))
+
+if (( ${#source_i[@]} > 0 )); then
+  e_header "Adding APT sources (${#source_i[@]})"
+  for i in "${source_i[@]}"; do
+    source_file=${apt_source_files[i]}
+    source_text=${apt_source_texts[i]}
+    if [[ "$source_text" =~ ppa: ]]; then
+      e_info "$source_text"
+      sudo add-apt-repository -y $source_text
+    else
+      e_info "$source_file"
+      sudo sh -c "echo '$source_text' > /etc/apt/sources.list.d/$source_file.list"
+    fi
+  done
+fi
 
 # Update APT.
 e_info "Updating APT"
--- install/util.sh
+++ install/util.sh
@@ -96,4 +96,75 @@ function setdiff() {
   [[ "$1" ]] && echo "${setdiff_out[@]}"
 }
 
+# bash array operation {{{
+# Array mapper. Calls map_fn for each item ($1) and index ($2) in array, and
+# prints whatever map_fn prints. If map_fn is omitted, all input array items
+# are printed.
+# Usage: array_map array_name [map_fn]
+function array_map() {
+  local __i__ __val__ __arr__=$1; shift
+  for __i__ in $(eval echo "\${!$__arr__[@]}"); do
+    __val__="$(eval echo "\"\${$__arr__[__i__]}\"")"
+    if [[ "$1" ]]; then
+      "$@" "$__val__" $__i__
+    else
+      echo "$__val__"
+    fi
+  done
+}
+
+# Print bash array in the format "i <val>" (one per line) for debugging.
+function array_print() { array_map $1 __array_print; }
+function __array_print() { echo "$2 <$1>"; }
+
+# Array filter. Calls filter_fn for each item ($1) and index ($2) in array_name
+# array, and prints all values for which filter_fn returns a non-zero exit code
+# to stdout. If filter_fn is omitted, input array items that are empty strings
+# will be removed.
+# Usage: array_filter array_name [filter_fn]
+# Eg. mapfile filtered_arr < <(array_filter source_arr)
+function array_filter() { __array_filter 1 "$@"; }
+# Works like array_filter, but outputs array indices instead of array items.
+function array_filter_i() { __array_filter 0 "$@"; }
+# The core function. Wheeeee.
+function __array_filter() {
+  local __i__ __val__ __mode__ __arr__
+  __mode__=$1; shift; __arr__=$1; shift
+  for __i__ in $(eval echo "\${!$__arr__[@]}"); do
+    __val__="$(eval echo "\${$__arr__[__i__]}")"
+    if [[ "$1" ]]; then
+      "$@" "$__val__" $__i__ >/dev/null
+    else
+      [[ "$__val__" ]]
+    fi
+    if [[ "$?" == 0 ]]; then
+      if [[ $__mode__ == 1 ]]; then
+        eval echo "\"\${$__arr__[__i__]}\""
+      else
+        echo $__i__
+      fi
+    fi
+  done
+}
+
+# Array join. Joins array ($1) items on string ($2).
+function array_join() { __array_join 1 "$@"; }
+# Works like array_join, but removes empty items first.
+function array_join_filter() { __array_join 0 "$@"; }
+
+function __array_join() {
+  local __i__ __val__ __out__ __init__ __mode__ __arr__
+  __mode__=$1; shift; __arr__=$1; shift
+  for __i__ in $(eval echo "\${!$__arr__[@]}"); do
+    __val__="$(eval echo "\"\${$__arr__[__i__]}\"")"
+    if [[ $__mode__ == 1 || "$__val__" ]]; then
+      [[ "$__init__" ]] && __out__="$__out__$@"
+      __out__="$__out__$__val__"
+      __init__=1
+    fi
+  done
+  [[ "$__out__" ]] && echo "$__out__"
+}
+#}}}
+
 function version_gt() { test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"; }

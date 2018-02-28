37d702356e638582e1b0928767b33ddbb5cc3c80
Yang Yang <kiooss@gmail.com>
2018-03-01 00:24:09 +0900

    Update zsh config.

--- zsh/lib/post_oh_my_zsh.zsh
+++ zsh/lib/post_oh_my_zsh.zsh
@@ -18,3 +18,11 @@ ZSH_HIGHLIGHT_STYLES[hashed-command]='fg=green,bold'
 # the composer plugin create the c alias but I do not need it.
 unalias c 2>/dev/null
 unalias g 2>/dev/null
+
+# filename completion as a default when other completions fail
+# zstyle ':completion:*' completer _complete _ignored _files
+
+zle -C complete-file complete-word _generic
+zstyle ':completion:complete-file::::' completer _files
+bindkey '^xF' complete-file
+

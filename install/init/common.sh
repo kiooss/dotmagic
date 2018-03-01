7a073459e71e29d8f86e73786946d992d4da5af8
Yang Yang <kiooss@gmail.com>
2018-03-01 14:03:45 +0900

    Add rbenv install.

--- install/init/common.sh
+++ install/init/common.sh
@@ -7,3 +7,24 @@ else
     e_info "Installing on-my-zsh"
     git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 fi
+
+# install rbenv
+RBENV_ROOT="$HOME/.rbenv"
+if [ -d "$RBENV_ROOT" ]; then
+    e_error "rbenv already exists... Skipping."
+else
+    e_info "Installing rbenv"
+    git clone https://github.com/rbenv/rbenv.git "$RBENV_ROOT"
+    cd "$RBENV_ROOT" && src/configure && make -C src
+
+    git clone https://github.com/carsomyr/rbenv-bundler.git "$RBENV_ROOT"/plugins/bundler
+    git clone https://github.com/rbenv/ruby-build.git "$RBENV_ROOT"/plugins/ruby-build
+    git clone https://github.com/rbenv/rbenv-default-gems.git "$RBENV_ROOT"/plugins/rbenv-default-gems
+    cat <<EOT >> "$RBENV_ROOT"/default-gems
+bundler
+awesome_print
+pry
+lolcat
+git-up
+EOT
+fi

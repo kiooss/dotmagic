66859a0b289246c58083655512746579ec916219
Yang Yang <kiooss@gmail.com>
2018-03-17 11:07:06 +0900

    Set SUDO_ASKPASS for macos.

---
+++ bin/macos_sudo_askpass.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+pw="$(osascript -e 'Tell application "System Events" to display dialog "Password:" default answer "" with hidden answer' -e 'text returned of result' 2>/dev/null)" && echo "$pw"

a53a1e85e35e1dd03d2682fb33dfacbf0585be4d
Yang Yang <kiooss@gmail.com>
2019-03-05 17:27:53 +0900

    Use python3.6

--- link/vim.symlink/venv.sh
+++ link/vim.symlink/venv.sh
@@ -3,14 +3,18 @@
 # virtual environments for neovim.
 # pip2 install --user virtualenv
 
+set -ex
+
 # Declare a base path for both virtual environments
 venv="${XDG_CACHE_HOME:-$HOME/.cache}/vim/venv"
+
+PY3=python3.6
 
 # Try to detect python2/3 executables
 if ! hash python2 2>/dev/null; then
 	echo "Python2 installation not found."
 	exit 1
-elif ! hash python3 2>/dev/null; then
+elif ! hash $PY3 2>/dev/null; then
 	echo "Python3 installation not found."
 	exit 1
 fi
@@ -18,7 +22,7 @@ fi
 # Ensure python 2/3 virtual environments
 [ -d "$venv" ] || mkdir -p "$venv"
 [ -d "$venv/neovim2" ] || python2 -m virtualenv "$venv/neovim2"
-[ -d "$venv/neovim3" ] || python3 -m venv "$venv/neovim3"
+[ -d "$venv/neovim3" ] || $PY3 -m venv "$venv/neovim3"
 
 # Install or upgrade dependencies
 echo ':: PYTHON 2'
@@ -26,4 +30,5 @@ echo ':: PYTHON 2'
 "$venv/neovim2/bin/pip" install -U neovim PyYAML solargraph-utils.py
 echo -e '\n:: PYTHON 3'
 "$venv/neovim3/bin/pip" install -U pip
-"$venv/neovim3/bin/pip" install -U neovim PyYAML solargraph-utils.py
+# "$venv/neovim3/bin/pip" install -U neovim PyYAML solargraph-utils.py
+"$venv/neovim3/bin/pip" install -U neovim

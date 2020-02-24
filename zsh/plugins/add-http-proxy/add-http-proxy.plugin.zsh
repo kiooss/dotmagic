1a9ff9b4c82eae45827fa8595159beae5d0281ee
Yang Yang <kiooss@gmail.com>
2020-02-24 17:06:00 +0800

    Update.

--- zsh/plugins/add-http-proxy/add-http-proxy.plugin.zsh
+++ zsh/plugins/add-http-proxy/add-http-proxy.plugin.zsh
@@ -2,9 +2,9 @@
 add-http-proxy-command-line() {
     [[ -z $BUFFER ]] && zle up-history
     if [[ $BUFFER == http_proxy=*\ * ]]; then
-        LBUFFER="${LBUFFER#env ALL_PROXY=socks5h://$MY_PROXY_HOST:7891 }"
+        LBUFFER="${LBUFFER#env ALL_PROXY=socks5h://${MY_PROXY_HOST:-localhost}:7891 }"
     else
-        LBUFFER="env ALL_PROXY=socks5h://$MY_PROXY_HOST:7891 $LBUFFER"
+        LBUFFER="env ALL_PROXY=socks5h://${MY_PROXY_HOST:-localhost}:7891 $LBUFFER"
     fi
 }
 zle -N add-http-proxy-command-line

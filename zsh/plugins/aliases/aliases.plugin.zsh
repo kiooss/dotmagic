470189575550f6c7d45565f27455e37249a485b7
Yang Yang <kiooss@gmail.com>
2021-08-06 17:26:55 +0900

    Update alias.

--- zsh/plugins/aliases/aliases.plugin.zsh
+++ zsh/plugins/aliases/aliases.plugin.zsh
@@ -34,7 +34,7 @@ alias du='du -h -c' # calculate disk usage for a folder
 alias groot='cd $(git rev-parse --show-toplevel 2> /dev/null || echo -n ".")'
 
 # IP addresses
-alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias myip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias localip="ipconfig getifaddr en1"
 alias ips="ifconfig -a | perl -nle'/(\d+\.\d+\.\d+\.\d+)/ && print $1'"
 

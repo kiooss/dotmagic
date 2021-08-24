515f2a94a919f7cf72bdbe15ea6aaffdb1ed7bc6
Yang Yang <kiooss@gmail.com>
2021-08-24 12:49:48 +0900

    zsh config.

--- zsh/lib/post_oh_my_zsh.zsh
+++ zsh/lib/post_oh_my_zsh.zsh
@@ -2,19 +2,20 @@
 ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
 bindkey '^ ' autosuggest-accept
 
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=cyan'
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=118'
+ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 # }}}
 
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
 ZSH_HIGHLIGHT_STYLES[cursor]='bold'
 
 ZSH_HIGHLIGHT_STYLES[alias]='fg=green,bold'
-ZSH_HIGHLIGHT_STYLES[suffix-alias]='fg=green,bold'
-ZSH_HIGHLIGHT_STYLES[builtin]='fg=green,bold'
-ZSH_HIGHLIGHT_STYLES[function]='fg=green,bold'
-ZSH_HIGHLIGHT_STYLES[command]='fg=green,bold'
-ZSH_HIGHLIGHT_STYLES[precommand]='fg=green,bold'
-ZSH_HIGHLIGHT_STYLES[hashed-command]='fg=green,bold'
+ZSH_HIGHLIGHT_STYLES[suffix-alias]='fg=magenta,bold'
+ZSH_HIGHLIGHT_STYLES[builtin]='fg=blue,bold'
+ZSH_HIGHLIGHT_STYLES[function]='fg=cyan,bold'
+ZSH_HIGHLIGHT_STYLES[command]='fg=yellow,bold'
+ZSH_HIGHLIGHT_STYLES[precommand]='fg=lightgreen,bold'
+ZSH_HIGHLIGHT_STYLES[hashed-command]='fg=lightblue,bold'
 
 # the composer plugin create the c alias but I do not need it.
 unalias c 2>/dev/null
@@ -25,9 +26,34 @@ unalias rg 2>/dev/null
 # filename completion as a default when other completions fail
 # zstyle ':completion:*' completer _complete _ignored _files
 
-zle -C complete-file complete-word _generic
-zstyle ':completion:complete-file::::' completer _files
-bindkey '^xF' complete-file
+# zle -C complete-file complete-word _generic
+# zstyle ':completion:complete-file::::' completer _files
+# bindkey '^xF' complete-file
 
 # fix paste slow issue
 zstyle ':bracketed-paste-magic' active-widgets '.self-*'
+
+zstyle ':completion:*' verbose yes
+
+# zstyle ':completion:*:descriptions' format '%B%d%b'
+zstyle ':completion:*:descriptions' format '%F{yellow}留%f%F{green}%d%f'
+zstyle ':completion:*:messages' format '%F{yellow}留%f%F{green}%d%f'
+zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
+
+# Grouping Results
+zstyle ':completion:*' group-name ''
+
+zstyle ':completion:*' menu select
+zstyle ':completion:*:*:default' force-list always
+zstyle ':completion:*' select-prompt '%SSelect:  lines: %L  matches: %M  [%p]'
+
+zstyle ':completion:*:match:*' original only
+zstyle ':completion::prefix-1:*' completer _complete
+zstyle ':completion:predict:*' completer _complete
+zstyle ':completion:incremental:*' completer _complete _correct
+zstyle ':completion:*' completer _complete _prefix _correct _match _approximate
+
+#路径补全
+zstyle ':completion:*' expand 'yes'
+zstyle ':completion:*' squeeze-shlashes 'yes'
+zstyle ':completion::complete:*' '\\'

9c454da156c735fcf3ecc9e772e8e1ba9e53c37a
Yang Yang <kiooss@gmail.com>
2021-11-11 15:39:56 +0800

    feat(bin): update-neovim.

--- bin/update-neovim.sh
+++ bin/update-neovim.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
 
-killall -q nvim
-curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage -o "$HOME/bin/nvim.appimage"
+set -x
+
+killall nvim
+
+if [[ $OSTYPE == 'darwin'* ]]; then
+  echo 'macOS'
+  cd "$HOME/bin" || exit
+  curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz -o nvim-macos.tar.gz
+  curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz.sha256sum -o nvim-macos.tar.gz.sha256sum
+  sha256sum --check nvim-macos.tar.gz.sha256sum || exit
+  tar xzvf nvim-macos.tar.gz
+  ln -sfn ./nvim-osx64/bin/nvim nvim
+else
+  echo 'Linux'
+  cd "$HOME/bin" || exit
+  curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage -o nvim.appimage
+  curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage.sha256sum -o nvim.appimage.sha256sum
+  sha256sum --check nvim.appimage.sha256sum || exit
+fi

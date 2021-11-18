cb7a73dc71514f32e2857836e39fa1134779c673
Yang Yang <kiooss@gmail.com>
2021-11-18 11:40:48 +0900

    feat(nvim): packer config.

--- dotconfig/nvim/lua/plugins.lua
+++ dotconfig/nvim/lua/plugins.lua
@@ -8,9 +8,9 @@ local config = {
     threshold = 0, -- the amount in ms that a plugins load time must be over for it to be included in the profile
   },
   display = {
-    open_fn = function()
-      return require('packer.util').float({ border = 'single' })
-    end,
+    -- open_fn = function()
+    --   return require('packer.util').float({ border = 'single' })
+    -- end,
   },
   -- list of plugins that should be taken from ~/projects
   -- this is NOT packer functionality!

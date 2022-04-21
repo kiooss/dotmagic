1524bb5d80cf727db3b5dada8c87c9146780dcc7
Yang Yang <kiooss@gmail.com>
2022-04-21 14:39:17 +0900

    feat(nvim): cmp config.

--- dotconfig/nvim/lua/config/nvim-cmp.lua
+++ dotconfig/nvim/lua/config/nvim-cmp.lua
@@ -32,6 +32,7 @@ cmp.setup({
       c = cmp.mapping.close(),
     }),
     ['<CR>'] = cmp.mapping.confirm({
+      -- default is cmp.CompeConfirmBehavior.Insert
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     }),
@@ -61,15 +62,24 @@ cmp.setup({
     format = require('lspkind').cmp_format({
       with_text = true,
       menu = {
-        look = '',
-        nvim_lsp = '',
-        nvim_lua = '',
-        treesitter = '',
-        path = '',
-        buffer = '﬘',
-        zsh = '',
-        vsnip = '',
-        spell = '暈',
+        look = '[look]',
+        nvim_lsp = '[LSP]',
+        nvim_lua = '[api]',
+        treesitter = '[treesitter]',
+        path = '[path]',
+        buffer = '[buffer]',
+        zsh = '[zsh]',
+        vsnip = '[vsnip]',
+        spell = '[spell]',
+        -- look = '',
+        -- nvim_lsp = '',
+        -- nvim_lua = '',
+        -- treesitter = '',
+        -- path = '',
+        -- buffer = '﬘',
+        -- zsh = '',
+        -- vsnip = '',
+        -- spell = '暈',
       },
     }),
   },
@@ -85,6 +95,33 @@ cmp.setup({
     { name = 'path' },
     { name = 'look', keyword_length = 2, option = { convert_case = true, loud = true } },
   }),
+
+  sorting = {
+    comparators = {
+      cmp.config.compare.offset,
+      cmp.config.compare.exact,
+      cmp.config.compare.score,
+
+      -- copied from cmp-under, but I don't think I need the plugin for this.
+      -- I might add some more of my own.
+      function(entry1, entry2)
+        local _, entry1_under = entry1.completion_item.label:find('^_+')
+        local _, entry2_under = entry2.completion_item.label:find('^_+')
+        entry1_under = entry1_under or 0
+        entry2_under = entry2_under or 0
+        if entry1_under > entry2_under then
+          return false
+        elseif entry1_under < entry2_under then
+          return true
+        end
+      end,
+
+      cmp.config.compare.kind,
+      cmp.config.compare.sort_text,
+      cmp.config.compare.length,
+      cmp.config.compare.order,
+    },
+  },
 
   experimental = {
     -- native_menu = true,

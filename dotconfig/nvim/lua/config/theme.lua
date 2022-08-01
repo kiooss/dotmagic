f19d149baea17ff0edd5f56411ab4446d15c4449
Yang Yang <kiooss@gmail.com>
2022-08-01 14:36:09 +0900

    feat(nvim): config.

--- dotconfig/nvim/lua/config/theme.lua
+++ dotconfig/nvim/lua/config/theme.lua
@@ -85,72 +85,10 @@ function config.nightfox()
 end
 
 function config.catppuccin()
+  vim.g.catppuccin_flavour = 'macchiato' -- latte, frappe, macchiato, mocha
   local catppuccin = require('catppuccin')
-
   -- configure it
-  catppuccin.setup({
-    transparent_background = false,
-    term_colors = false,
-    styles = {
-      comments = 'italic',
-      functions = 'italic',
-      keywords = 'bold',
-      strings = 'NONE',
-      variables = 'italic',
-    },
-    integrations = {
-      treesitter = true,
-      native_lsp = {
-        enabled = true,
-        virtual_text = {
-          errors = 'italic',
-          hints = 'italic',
-          warnings = 'italic',
-          information = 'italic',
-        },
-        underlines = {
-          errors = 'undercurl',
-          hints = 'undercurl',
-          warnings = 'undercurl',
-          information = 'undercurl',
-        },
-      },
-      lsp_trouble = false,
-      cmp = true,
-      lsp_saga = false,
-      gitgutter = false,
-      gitsigns = true,
-      telescope = true,
-      nvimtree = {
-        enabled = true,
-        show_root = false,
-        transparent_panel = true,
-      },
-      neotree = {
-        enabled = false,
-        show_root = false,
-        transparent_panel = false,
-      },
-      which_key = true,
-      indent_blankline = {
-        enabled = true,
-        colored_indent_levels = true,
-      },
-      dashboard = true,
-      neogit = false,
-      vim_sneak = false,
-      fern = false,
-      barbar = false,
-      bufferline = true,
-      markdown = true,
-      lightspeed = false,
-      ts_rainbow = false,
-      hop = false,
-      notify = true,
-      telekasten = true,
-      symbols_outline = true,
-    },
-  })
+  catppuccin.setup({})
 end
 
 function config.setup(theme)

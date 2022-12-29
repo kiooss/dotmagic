a7bde07806a57504141d6d75a4ed8304f7f30eb1
Yang Yang <kiooss@gmail.com>
2022-12-29 17:56:29 +0900

    feat(nvim): update.

--- dotconfig/nvim/lua/config/events.lua
+++ dotconfig/nvim/lua/config/events.lua
@@ -14,12 +14,6 @@ end
 -- lua api
 local group = vim.api.nvim_create_augroup('kiooss', { clear = true })
 
-vim.api.nvim_create_autocmd('BufWritePost', {
-  pattern = 'plugins.lua',
-  command = 'source <afile>',
-  group = group,
-})
-
 vim.api.nvim_create_autocmd({ 'WinEnter', 'BufEnter', 'InsertLeave' }, {
   command = [[if ! &cursorline && &filetype !~# '^\(dashboard\|clap_\)' && ! &pvw | setlocal cursorline | endif]],
   group = group,
@@ -132,4 +126,17 @@ vim.api.nvim_create_autocmd('CursorHold', {
     vim.diagnostic.open_float(nil, { scope = 'cursor', focusable = false, border = 'rounded' })
   end,
   group = group,
+})
+
+-- create directories when needed, when saving a file
+vim.api.nvim_create_autocmd('BufWritePre', {
+  group = vim.api.nvim_create_augroup('auto_create_dir', { clear = true }),
+  callback = function(event)
+    local file = vim.loop.fs_realpath(event.match) or event.match
+
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ':p:h'), 'p')
+    local backup = vim.fn.fnamemodify(file, ':p:~:h')
+    backup = backup:gsub('[/\\]', '%%')
+    vim.go.backupext = backup
+  end,
 })

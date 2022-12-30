285b05d4843dbc533aec427fa04d0730ea9bc286
Yang Yang <kiooss@gmail.com>
2022-12-30 16:01:44 +0900

    feat(lualine): update.

--- dotconfig/nvim/lua/config/plugins/lualine.lua
+++ dotconfig/nvim/lua/config/plugins/lualine.lua
@@ -1,10 +1,10 @@
 local M = {
-  "nvim-lualine/lualine.nvim",
-  event = "VeryLazy",
+  'nvim-lualine/lualine.nvim',
+  event = 'VeryLazy',
 }
 
 local function clock()
-  return " " .. os.date("%H:%M")
+  return ' ' .. os.date('%H:%M')
 end
 
 function M.config()
@@ -12,35 +12,35 @@ function M.config()
     return
   end
 
-  require("lualine").setup({
+  require('lualine').setup({
     options = {
-      theme = "auto",
-      section_separators = { left = "", right = "" },
-      component_separators = { left = "", right = "" },
+      theme = 'auto',
+      section_separators = { left = '', right = '' },
+      component_separators = { left = '', right = '' },
       icons_enabled = true,
       globalstatus = true,
-      disabled_filetypes = { statusline = { "dashboard", "lazy" } },
+      disabled_filetypes = { statusline = { 'dashboard', 'lazy' } },
     },
     sections = {
-      lualine_a = { { "mode", separator = { left = "" } } },
-      lualine_b = { "branch" },
+      lualine_a = { { 'mode', separator = { left = '' } } },
+      lualine_b = { 'branch' },
       lualine_c = {
-        { "diagnostics", sources = { "nvim_diagnostic" } },
-        { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-        { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
+        { 'diagnostics', sources = { 'nvim_diagnostic' } },
+        { 'filetype', icon_only = true, separator = '', padding = { left = 1, right = 0 } },
+        { 'filename', path = 1, symbols = { modified = '  ', readonly = '', unnamed = '' } },
         {
           function()
-            local navic = require("nvim-navic")
+            local navic = require('nvim-navic')
             local ret = navic.get_location()
-            return ret:len() > 2000 and "navic error" or ret
+            return ret:len() > 2000 and 'navic error' or ret
           end,
           cond = function()
-            if package.loaded["nvim-navic"] then
-              local navic = require("nvim-navic")
+            if package.loaded['nvim-navic'] then
+              local navic = require('nvim-navic')
               return navic.is_available()
             end
           end,
-          color = { fg = "#ff9e64" },
+          color = { fg = '#ff9e64' },
         },
       },
       lualine_x = {
@@ -50,63 +50,63 @@ function M.config()
         -- },
         {
           function()
-            return require("noice").api.status.command.get()
+            return require('noice').api.status.command.get()
           end,
           cond = function()
-            if package.loaded["noice"] then
-              return require("noice").api.status.command.has()
+            if package.loaded['noice'] then
+              return require('noice').api.status.command.has()
             end
           end,
-          color = { fg = "#ff9e64" },
+          color = { fg = '#ff9e64' },
         },
         {
           function()
-            return require("noice").api.status.mode.get()
+            return require('noice').api.status.mode.get()
           end,
           cond = function()
-            if package.loaded["noice"] then
-              return require("noice").api.status.mode.has()
+            if package.loaded['noice'] then
+              return require('noice').api.status.mode.has()
             end
           end,
-          color = { fg = "#ff9e64" },
+          color = { fg = '#ff9e64' },
         },
         {
           function()
-            return require("noice").api.status.search.get()
+            return require('noice').api.status.search.get()
           end,
           cond = function()
-            if package.loaded["noice"] then
-              return require("noice").api.status.search.has()
+            if package.loaded['noice'] then
+              return require('noice').api.status.search.has()
             end
           end,
-          color = { fg = "#ff9e64" },
+          color = { fg = '#ff9e64' },
         },
         {
           function()
-            return require("lazy.status").updates()
+            return require('lazy.status').updates()
           end,
-          cond = require("lazy.status").has_updates,
-          color = { fg = "#ff9e64" },
+          cond = require('lazy.status').has_updates,
+          color = { fg = '#ff9e64' },
         },
         {
           function()
-            local stats = require("lazy").stats()
+            local stats = require('lazy').stats()
             local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-            return " " .. ms .. "ms"
+            return ' ' .. ms .. 'ms'
           end,
-          color = { fg = "#ff9e64" },
+          color = { fg = '#ff9e64' },
         },
         -- function()
         --   return require("messages.view").status
         -- end,
-        {
-          function()
-            return require("util.dashboard").status()
-          end,
-        },
+        -- {
+        --   function()
+        --     return require("util.dashboard").status()
+        --   end,
+        -- },
       },
-      lualine_y = { "location" },
-      lualine_z = { { clock, separator = { right = "" } } },
+      lualine_y = { 'location' },
+      lualine_z = { { clock, separator = { right = '' } } },
     },
     inactive_sections = {
       lualine_a = {},
@@ -133,7 +133,7 @@ function M.config()
     --   lualine_y = {},
     --   lualine_z = {},
     -- },
-    extensions = { "nvim-tree" },
+    extensions = { 'nvim-tree' },
   })
 end
 

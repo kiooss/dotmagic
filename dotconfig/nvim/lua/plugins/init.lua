1393619819c5fb1176b12c3f13a917ae7a4977a9
Yang Yang <kiooss@gmail.com>
2023-01-01 13:47:45 +0900

    feat(nvim): update.

--- dotconfig/nvim/lua/plugins/init.lua
+++ dotconfig/nvim/lua/plugins/init.lua
@@ -1,40 +1,40 @@
 return {
-  'b0o/SchemaStore.nvim',
-  'jose-elias-alvarez/typescript.nvim',
-  'folke/neodev.nvim',
-  'MunifTanjim/nui.nvim',
-  'williamboman/mason-lspconfig.nvim',
-  'nvim-lua/plenary.nvim',
-  'windwp/nvim-spectre',
-  'folke/twilight.nvim',
-  'folke/which-key.nvim',
+  "b0o/SchemaStore.nvim",
+  "jose-elias-alvarez/typescript.nvim",
+  "folke/neodev.nvim",
+  "MunifTanjim/nui.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  "nvim-lua/plenary.nvim",
+  "windwp/nvim-spectre",
+  "folke/twilight.nvim",
+  "folke/which-key.nvim",
 
-  { 'editorconfig/editorconfig-vim', event = 'VeryLazy' },
-  { 'AndrewRadev/switch.vim', keys = { 'gs' } },
-  { 'rhysd/committia.vim', lazy = false },
+  { "editorconfig/editorconfig-vim", event = "VeryLazy" },
+  { "AndrewRadev/switch.vim", keys = { "gs" } },
+  { "rhysd/committia.vim", lazy = false },
 
   -- additional text objects
-  { 'wellle/targets.vim', lazy = false },
-  { 'kana/vim-textobj-user', lazy = false, priority = 99 },
-  { 'kana/vim-textobj-entire', lazy = false },
-  { 'kana/vim-textobj-function', lazy = false },
-  { 'rhysd/vim-textobj-anyblock', lazy = false },
-  { 'nelstrom/vim-textobj-rubyblock', lazy = false },
-  { 'thalesmello/vim-textobj-methodcall', lazy = false },
+  { "wellle/targets.vim", lazy = false },
+  { "kana/vim-textobj-user", lazy = false, priority = 99 },
+  { "kana/vim-textobj-entire", lazy = false },
+  { "kana/vim-textobj-function", lazy = false },
+  { "rhysd/vim-textobj-anyblock", lazy = false },
+  { "nelstrom/vim-textobj-rubyblock", lazy = false },
+  { "thalesmello/vim-textobj-methodcall", lazy = false },
 
-  { 'folke/neoconf.nvim', cmd = 'Neoconf' },
+  { "folke/neoconf.nvim", cmd = "Neoconf" },
 
-  { 'shaunsingh/oxocarbon.nvim', lazy = false, enabled = true },
+  { "shaunsingh/oxocarbon.nvim", lazy = false, enabled = true },
 
-  { 'ellisonleao/gruvbox.nvim', lazy = false },
+  { "ellisonleao/gruvbox.nvim", lazy = false },
 
-  { 'tjdevries/cyclist.vim', lazy = false },
+  { "tjdevries/cyclist.vim", lazy = false },
 
   {
-    'folke/trouble.nvim',
-    cmd = { 'TroubleToggle', 'Trouble' },
+    "folke/trouble.nvim",
+    cmd = { "TroubleToggle", "Trouble" },
     config = function()
-      require('trouble').setup({
+      require("trouble").setup({
         auto_open = false,
         use_diagnostic_signs = true, -- en
       })
@@ -42,12 +42,12 @@ return {
   },
 
   {
-    'folke/styler.nvim',
-    event = 'VeryLazy',
+    "folke/styler.nvim",
+    event = "VeryLazy",
     config = function()
-      require('styler').setup({
+      require("styler").setup({
         themes = {
-          markdown = { colorscheme = 'tokyonight-storm' },
+          markdown = { colorscheme = "tokyonight-storm" },
           -- help = { colorscheme = 'oxocarbon', background = 'dark' },
           -- noice = { colorscheme = "gruvbox", background = "dark" },
         },
@@ -56,13 +56,13 @@ return {
   },
 
   {
-    'folke/drop.nvim',
-    event = 'VimEnter',
+    "folke/drop.nvim",
+    event = "VimEnter",
     enabled = true,
     config = function()
       math.randomseed(os.time())
-      local theme = ({ 'stars', 'snow', 'xmas' })[math.random(1, 3)]
-      require('drop').setup({ theme = theme })
+      local theme = ({ "stars", "snow", "xmas" })[math.random(1, 3)]
+      require("drop").setup({ theme = theme })
     end,
   },
 
@@ -74,105 +74,121 @@ return {
   -- },
 
   {
-    'ruanyl/vim-gh-line',
-    keys = { '<leader>gm' },
+    "ruanyl/vim-gh-line",
+    keys = { "<leader>gm" },
     config = function()
       vim.g.gh_trace = 1
-      vim.g.gh_open_command = 'echo '
+      vim.g.gh_open_command = "echo "
       vim.g.gh_use_canonical = 0
-      vim.g.gh_line_blame_map = '<leader>gm'
+      vim.g.gh_line_blame_map = "<leader>gm"
     end,
   },
 
   {
-    'danymat/neogen',
+    "danymat/neogen",
     config = function()
-      require('neogen').setup({ snippet_engine = 'luasnip' })
+      require("neogen").setup({ snippet_engine = "luasnip" })
     end,
   },
 
   -- Theme: icons
   {
-    'nvim-tree/nvim-web-devicons',
+    "nvim-tree/nvim-web-devicons",
     config = function()
-      require('nvim-web-devicons').setup({ default = true })
+      require("nvim-web-devicons").setup({ default = true })
     end,
   },
 
   {
-    'smjonas/inc-rename.nvim',
-    cmd = 'IncRename',
+    "smjonas/inc-rename.nvim",
+    cmd = "IncRename",
     config = function()
-      require('inc_rename').setup()
+      require("inc_rename").setup()
     end,
   },
 
   {
-    'dstein64/vim-startuptime',
-    cmd = 'StartupTime',
+    "dstein64/vim-startuptime",
+    cmd = "StartupTime",
     config = function()
       vim.g.startuptime_tries = 10
     end,
   },
 
   {
-    'nvim-treesitter/nvim-treesitter-context',
-    event = 'BufReadPre',
+    "nvim-treesitter/nvim-treesitter-context",
+    event = "BufReadPre",
     config = function()
-      require('treesitter-context').setup()
+      require("treesitter-context").setup()
     end,
   },
 
   {
-    'SmiteshP/nvim-navic',
+    "SmiteshP/nvim-navic",
     config = function()
       vim.g.navic_silence = true
-      require('nvim-navic').setup({ separator = ' ', highlight = true, depth_limit = 5 })
+      require("nvim-navic").setup({ separator = " ", highlight = true, depth_limit = 5 })
     end,
   },
 
   {
-    'andymass/vim-matchup',
-    event = 'BufReadPost',
+    "andymass/vim-matchup",
+    event = "BufReadPost",
     config = function()
-      vim.g.matchup_matchparen_offscreen = { method = 'status_manual' }
+      vim.g.matchup_matchparen_offscreen = { method = "status_manual" }
     end,
   },
 
   {
-    'lambdalisue/suda.vim',
-    cmd = { 'SudaWrite', 'SudaRead' },
+    "lambdalisue/suda.vim",
+    cmd = { "SudaWrite", "SudaRead" },
   },
 
   {
-    'pechorin/any-jump.vim',
-    cmd = { 'AnyJump', 'AnyJumpVisual' },
+    "pechorin/any-jump.vim",
+    cmd = { "AnyJump", "AnyJumpVisual" },
   },
 
   {
-    'Wansmer/treesj',
+    "Wansmer/treesj",
     keys = {
-      { 'J', '<cmd>TSJToggle<cr>' },
+      { "J", "<cmd>TSJToggle<cr>" },
     },
     config = { use_default_keymaps = false },
   },
 
-  { 'mhinz/vim-sayonara', cmd = { 'Sayonara' } },
+  { "mhinz/vim-sayonara", cmd = { "Sayonara" } },
 
   {
-    'vimwiki/vimwiki',
-    cmd = 'VimwikiIndex',
-    keys = { '<leader>W' },
+    "stevearc/dressing.nvim",
+    init = function()
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.select = function(...)
+        require("lazy").load({ plugins = { "dressing.nvim" } })
+        return vim.ui.select(...)
+      end
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.input = function(...)
+        require("lazy").load({ plugins = { "dressing.nvim" } })
+        return vim.ui.input(...)
+      end
+    end,
+  },
+
+  {
+    "vimwiki/vimwiki",
+    cmd = "VimwikiIndex",
+    keys = { "<leader>W" },
     init = function()
       vim.g.vimwiki_list = {
-        { path = '~/vimwiki/', syntax = 'markdown', ext = '.md' },
+        { path = "~/vimwiki/", syntax = "markdown", ext = ".md" },
       }
       vim.g.vimwiki_conceallevel = 0
       vim.g.vimwiki_use_calendar = 1
       vim.g.vimwiki_hl_headers = 1
       vim.g.vimwiki_hl_cb_checked = 1
       vim.g.vimwiki_autowriteall = 0
-      vim.g.vimwiki_map_prefix = '<F12>'
+      vim.g.vimwiki_map_prefix = "<F12>"
       vim.g.vimwiki_table_mappings = 0
     end,
   },
--- dotconfig/nvim/lua/plugins/luasnip.lua
+++ dotconfig/nvim/lua/plugins/luasnip.lua
@@ -1,45 +1,45 @@
 -- TODO
 local M = {
-  'L3MON4D3/LuaSnip',
+  "L3MON4D3/LuaSnip",
 
   dependencies = {
-    'rafamadriz/friendly-snippets',
+    "rafamadriz/friendly-snippets",
     config = function()
-      require('luasnip.loaders.from_vscode').lazy_load()
+      require("luasnip.loaders.from_vscode").lazy_load()
     end,
   },
 }
 
 function M.config()
-  local luasnip = require('luasnip')
-  require('neogen')
+  local luasnip = require("luasnip")
+  require("neogen")
 
   luasnip.config.setup({
     history = true,
     enable_autosnippets = true,
     -- Update more often, :h events for more info.
-    updateevents = 'TextChanged,TextChangedI',
+    updateevents = "TextChanged,TextChangedI",
   })
 
-  luasnip.filetype_extend('all', { '_' })
-  luasnip.filetype_extend('ruby', { 'rails' })
+  luasnip.filetype_extend("all", { "_" })
+  luasnip.filetype_extend("ruby", { "rails" })
 
-  require('luasnip.loaders.from_lua').load({ paths = '~/.config/nvim/lua/yy/snippets' })
+  require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/yy/snippets" })
 
   -- keymaps
   -- <c-l> is selecting within a list of options.
   -- This is useful for choice nodes (introduced in the forthcoming episode 2)
-  vim.keymap.set('i', '<c-l>', function()
+  vim.keymap.set("i", "<c-l>", function()
     if luasnip.choice_active() then
       luasnip.change_choice(1)
     end
   end)
 
-  vim.keymap.set('i', '<c-u>', require('luasnip.extras.select_choice'))
+  vim.keymap.set("i", "<c-u>", require("luasnip.extras.select_choice"))
 
-  vim.keymap.set('n', '<leader>se', function()
-    require('luasnip.loaders.from_lua').edit_snippet_files()
-  end)
+  vim.keymap.set("n", "<leader>se", function()
+    require("luasnip.loaders.from_lua").edit_snippet_files()
+  end, { desc = "Edit snippet files" })
 end
 
 return M

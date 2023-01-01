acb50d3939fd30b304e2d3533c15bab72551acd5
Yang Yang <kiooss@gmail.com>
2023-01-01 21:08:02 +0900

    feat(nvim): update.

--- dotconfig/nvim/lua/plugins/mason.lua
+++ dotconfig/nvim/lua/plugins/mason.lua
@@ -1,23 +1,23 @@
 local M = {
-  'williamboman/mason.nvim',
+  "williamboman/mason.nvim",
 }
 
 M.tools = {
   -- "prettierd",
-  'stylua',
-  'selene',
-  'luacheck',
+  "stylua",
+  "selene",
+  -- 'luacheck',
   -- "eslint_d",
-  'shellcheck',
+  "shellcheck",
   -- "deno",
-  'shfmt',
+  "shfmt",
   -- "black",
   -- "isort",
   -- "flake8",
 }
 
 function M.check()
-  local mr = require('mason-registry')
+  local mr = require("mason-registry")
   for _, tool in ipairs(M.tools) do
     local p = mr.get_package(tool)
     if not p:is_installed() then
@@ -27,9 +27,9 @@ function M.check()
 end
 
 function M.config()
-  require('mason').setup()
+  require("mason").setup()
   M.check()
-  require('mason-lspconfig').setup({
+  require("mason-lspconfig").setup({
     automatic_installation = true,
   })
 end
--- dotconfig/nvim/lua/plugins/noice.lua
+++ dotconfig/nvim/lua/plugins/noice.lua
@@ -43,8 +43,8 @@ function M.config()
       },
     },
     presets = {
-      bottom_search = true,
-      command_palette = true,
+      bottom_search = false,
+      command_palette = false,
       long_message_to_split = true,
       inc_rename = true,
       cmdline_output_to_split = false,

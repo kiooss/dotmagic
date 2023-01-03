3a96a1cb51b00cf5832216f02a25ad4a974d3565
Yang Yang <kiooss@gmail.com>
2023-01-03 12:46:50 +0900

    feat(nvim): bufferline.

--- dotconfig/nvim/lua/plugins/bufferline.lua
+++ dotconfig/nvim/lua/plugins/bufferline.lua
@@ -26,6 +26,7 @@ function M.config()
       numbers = function(opts)
         return string.format("%s", opts.raise(opts.ordinal))
       end,
+      show_close_icon = false,
       show_buffer_close_icons = false,
       diagnostics = "nvim_lsp",
       separator_style = "slant",

33a447c0403b7f1af705fc942f943bb3d635e313
Yang Yang <kiooss@gmail.com>
2023-01-11 16:58:51 +0900

    feat(nvim): load nvim-tree when open directory directly.

--- dotconfig/nvim/lua/plugins/editor.lua
+++ dotconfig/nvim/lua/plugins/editor.lua
@@ -31,58 +31,63 @@ return {
     keys = {
       { "<c-p>", "<cmd>NvimTreeFindFileToggle<CR>", desc = "NvimTreeFindFileToggle" },
     },
-    config = function()
+    opts = {
+      -- disables netrw completely
+      disable_netrw = true,
+      hijack_netrw = true,
+      diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+        icons = {
+          hint = "",
+          info = "",
+          warning = "",
+          error = "",
+        },
+      },
+      -- hijack the cursor in the tree to put it at the start of the filename
+      hijack_cursor = true,
+      hijack_directories = {
+        enable = true,
+        auto_open = true,
+      },
+      update_cwd = true,
+      update_focused_file = {
+        enable = true,
+      },
+      git = {
+        enable = true,
+        ignore = true,
+        timeout = 500,
+      },
+      view = {
+        mappings = {
+          list = {
+            { key = "<tab>", cb = ":wincmd w<CR>" },
+            { key = "l", cb = ":lua require'nvim-tree'.on_keypress('edit')<CR>" },
+            { key = "s", cb = ":lua require'nvim-tree'.on_keypress('vsplit')<CR>" },
+            { key = "i", cb = ":lua require'nvim-tree'.on_keypress('split')<CR>" },
+          },
+        },
+      },
+      actions = {
+        open_file = {
+          resize_window = true,
+        },
+      },
+      renderer = {
+        highlight_git = true,
+        highlight_opened_files = "all",
+      },
+    },
+    init = function()
       vim.g.nvim_tree_auto_ignore_ft = { "dashboard", "startify" }
-
-      require("nvim-tree").setup({
-        -- disables netrw completely
-        disable_netrw = true,
-        hijack_netrw = true,
-        diagnostics = {
-          enable = true,
-          show_on_dirs = true,
-          icons = {
-            hint = "",
-            info = "",
-            warning = "",
-            error = "",
-          },
-        },
-        -- hijack the cursor in the tree to put it at the start of the filename
-        hijack_cursor = true,
-        hijack_directories = {
-          enable = true,
-          auto_open = true,
-        },
-        update_cwd = true,
-        update_focused_file = {
-          enable = true,
-        },
-        git = {
-          enable = true,
-          ignore = true,
-          timeout = 500,
-        },
-        view = {
-          mappings = {
-            list = {
-              { key = "<tab>", cb = ":wincmd w<CR>" },
-              { key = "l", cb = ":lua require'nvim-tree'.on_keypress('edit')<CR>" },
-              { key = "s", cb = ":lua require'nvim-tree'.on_keypress('vsplit')<CR>" },
-              { key = "i", cb = ":lua require'nvim-tree'.on_keypress('split')<CR>" },
-            },
-          },
-        },
-        actions = {
-          open_file = {
-            resize_window = true,
-          },
-        },
-        renderer = {
-          highlight_git = true,
-          highlight_opened_files = "all",
-        },
-      })
+      if vim.fn.argc() == 1 then
+        local stat = vim.loop.fs_stat(vim.fn.argv(0))
+        if stat and stat.type == "directory" then
+          require("nvim-tree")
+        end
+      end
     end,
   },
 

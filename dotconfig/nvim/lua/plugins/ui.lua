15d0c60fd7b8b6c85dd97600d25907f140dfe36d
Yang Yang <kiooss@gmail.com>
2023-01-12 11:58:52 +0900

    feat(ui): update.

--- dotconfig/nvim/lua/plugins/ui.lua
+++ dotconfig/nvim/lua/plugins/ui.lua
@@ -14,23 +14,7 @@ return {
         end,
         show_close_icon = false,
         show_buffer_close_icons = false,
-        diagnostics = "nvim_lsp",
         separator_style = "slant",
-        diagnostics_indicator = function(_, _, diag)
-          local icons = require("lazyvim.config.settings").icons.diagnostics
-          local ret = (diag.error and icons.Error .. diag.error .. " " or "")
-            .. (diag.warning and icons.Warn .. diag.warning or "")
-          return vim.trim(ret)
-        end,
-        -- diagnostics_indicator = function(_, _, diag)
-        --   local s = {}
-        --   for _, severity in ipairs(severities) do
-        --     if diag[severity] then
-        --       table.insert(s, signs[severity] .. diag[severity])
-        --     end
-        --   end
-        --   return table.concat(s, " ")
-        -- end,
         offsets = {
           {
             filetype = "NvimTree",

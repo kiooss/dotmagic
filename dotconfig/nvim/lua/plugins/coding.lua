dff20a9cd7145848dba0eb16c519b19e6dedb8e7
Yang Yang <kiooss@gmail.com>
2023-02-28 16:11:41 +0800

    feat(nvimtree): fix mappings.

--- dotconfig/nvim/lua/plugins/coding.lua
+++ dotconfig/nvim/lua/plugins/coding.lua
@@ -117,7 +117,7 @@ return {
   -- copilot
   {
     "zbirenbaum/copilot.lua",
-    enabled = true,
+    enabled = false,
     event = "VeryLazy",
     opts = {
       suggestion = {
@@ -147,7 +147,7 @@ return {
 
   {
     "github/copilot.vim",
-    enabled = false,
+    enabled = true,
     event = "VeryLazy",
     init = function()
       vim.g.copilot_no_tab_map = 1

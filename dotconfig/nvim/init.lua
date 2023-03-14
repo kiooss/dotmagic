be8e989c32fc57ba9610e8fe863ce96519c13c4e
Yang Yang <kiooss@gmail.com>
2023-03-14 14:36:31 +0800

    feat(nvim): update.

--- dotconfig/nvim/init.lua
+++ dotconfig/nvim/init.lua
@@ -8,9 +8,16 @@ if vim.env.VIMCONFIG then
   return debug.switch(vim.env.VIMCONFIG)
 end
 
-require("config.lazy")
-
--- require("util.dashboard").setup()
+require("config.lazy")({
+  defaults = {
+    lazy = true,
+  },
+  performance = {
+    cache = {
+      enabled = true,
+    },
+  },
+})
 
 vim.api.nvim_create_autocmd("User", {
   pattern = "VeryLazy",
--- dotconfig/nvim/lua/config/lazy.lua
+++ dotconfig/nvim/lua/config/lazy.lua
@@ -1,58 +1,60 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  -- bootstrap lazy.nvim
-  -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", lazypath })
+  vim.fn.system({ "git", "-C", lazypath, "checkout", "tags/stable" }) -- last stable release
 end
-vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-  spec = {
-    {
-      "LazyVim/LazyVim",
-      import = "lazyvim.plugins",
-      opts = {},
+---@param opts LazyConfig
+return function(opts)
+  opts = vim.tbl_deep_extend("force", {
+    spec = {
+      {
+        "LazyVim/LazyVim",
+        import = "lazyvim.plugins",
+        opts = {},
+      },
+      { import = "lazyvim.plugins.extras.lang.typescript" },
+      { import = "lazyvim.plugins.extras.lang.json" },
+      { import = "lazyvim.plugins.extras.ui.mini-animate" },
+      { import = "plugins" },
     },
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.ui.mini-animate" },
-    { import = "plugins" },
-  },
-  defaults = {
-    lazy = true, -- every plugin is lazy-loaded by default
-    version = "*", -- try installing the latest stable version for plugins that support semver
-  },
-  install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = {
-    enabled = true,
-    frequency = 3600 * 24, -- check for updates every day
-  },
-  diff = {
-    cmd = "terminal_git",
-  },
-  performance = {
-    cache = {
+    defaults = { lazy = true },
+    install = { colorscheme = { "tokyonight", "habamax" } },
+    checker = {
       enabled = true,
-      -- disable_events = {},
+      frequency = 3600 * 24, -- check for updates every day
     },
-    rtp = {
-      disabled_plugins = {
-        "gzip",
-        "matchit",
-        "matchparen",
-        "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
+    diff = {
+      cmd = "terminal_git",
+    },
+    performance = {
+      cache = {
+        enabled = true,
+        -- disable_events = {},
+      },
+      rtp = {
+        disabled_plugins = {
+          "gzip",
+          "matchit",
+          "matchparen",
+          "netrwPlugin",
+          "rplugin",
+          "tarPlugin",
+          "tohtml",
+          "tutor",
+          "zipPlugin",
+        },
       },
     },
-  },
-  ui = {
-    custom_keys = {
-      ["<localleader>d"] = function(plugin)
-        dd(plugin)
-      end,
+    ui = {
+      custom_keys = {
+        ["<localleader>d"] = function(plugin)
+          dd(plugin)
+        end,
+      },
     },
-  },
-})
+    debug = false,
+  }, opts or {})
+  require("lazy").setup(opts)
+end

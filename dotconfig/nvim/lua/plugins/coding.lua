da0eda3ec514b258742fd2465099d8bae1609458
Yang Yang <kiooss@gmail.com>
2023-05-17 08:18:36 +0800

    feat(nvim): update.

--- dotconfig/nvim/lua/plugins/coding.lua
+++ dotconfig/nvim/lua/plugins/coding.lua
@@ -144,17 +144,17 @@ return {
       },
     },
   },
-
-  {
-    "github/copilot.vim",
-    enabled = false,
-    event = "VeryLazy",
-    init = function()
-      vim.g.copilot_no_tab_map = 1
-      vim.g.copilot_filetypes = {
-        spectre_panel = false,
-      }
-      vim.keymap.set("i", "<c-j>", 'copilot#Accept("<CR>")', { expr = true })
-    end,
-  },
+  --
+  -- {
+  --   "github/copilot.vim",
+  --   enabled = false,
+  --   event = "VeryLazy",
+  --   init = function()
+  --     vim.g.copilot_no_tab_map = 1
+  --     vim.g.copilot_filetypes = {
+  --       spectre_panel = false,
+  --     }
+  --     vim.keymap.set("i", "<c-j>", 'copilot#Accept("<CR>")', { expr = true })
+  --   end,
+  -- },
 }

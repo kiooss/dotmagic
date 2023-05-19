667e3263ece145f866b5a9f1456c3071aaf73e21
Yang Yang <kiooss@gmail.com>
2023-05-19 13:13:54 +0800

    Back to use clipper. OSC 52 has limit size.

--- dotconfig/nvim/lua/plugins/tools.lua
+++ dotconfig/nvim/lua/plugins/tools.lua
@@ -39,6 +39,22 @@ return {
   --     vim.g.oscyank_term = "default"
   --   end,
   -- },
+  {
+    "wincent/vim-clipper",
+    lazy = false,
+    config = function()
+      vim.g.ClipperAddress = "~/.clipper.sock"
+      vim.g.ClipperPort = 0
+
+      if vim.fn.filereadable("/etc/arch-release") == 1 and vim.fn.executable("socat") == 1 then
+        vim.fn["clipper#set_invocation"]("socat - UNIX-CLIENT:" .. vim.fn.expand(vim.g.ClipperAddress))
+      elseif vim.fn.filereadable("/etc/debian_version") == 1 and vim.fn.executable("socat") == 1 then
+        vim.fn["clipper#set_invocation"]("socat - UNIX-CLIENT:" .. vim.fn.expand(vim.g.ClipperAddress))
+      else
+        vim.fn["clipper#set_invocation"]("")
+      end
+    end,
+  },
 
   {
     "ruifm/gitlinker.nvim",

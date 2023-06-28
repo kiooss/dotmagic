07e4c584b2dc8a1935d740ff0c04ddbabc84684c
Yang Yang <kiooss@gmail.com>
2023-06-28 20:54:57 +0800

    feat(nvim): vimwiki.

--- dotconfig/nvim/lua/plugins/misc.lua
+++ dotconfig/nvim/lua/plugins/misc.lua
@@ -44,6 +44,7 @@ return {
     cmd = "VimwikiIndex",
     keys = { { "<leader>W", "<cmd>VimwikiIndex<CR>", desc = "VimWiki" } },
     init = function()
+      vim.g.vimwiki_global_ext = 0
       vim.g.vimwiki_list = {
         { path = "~/vimwiki/", syntax = "markdown", ext = ".md" },
       }

ff13997fd89b625a529fa7f276be4c870c23e207
Yang Yang <kiooss@gmail.com>
2023-07-19 15:33:07 +0800

    feat(nvim): add iron.nvim

--- dotconfig/nvim/lua/plugins/tools.lua
+++ dotconfig/nvim/lua/plugins/tools.lua
@@ -66,4 +66,66 @@ return {
     cmd = { "ChatGPTActAs", "ChatGPT", "ChatGPTEditWithInstructions" },
     opts = {},
   },
+
+  {
+    "Vigemus/iron.nvim",
+    keys = {
+      { "<leader>rs", "<cmd>IronRepl<cr>", desc = "Iron Repl" },
+      { "<leader>rr", "<cmd>IronRestart<cr>", desc = "Iron Restart" },
+      { "<leader>rf", "<cmd>IronFocus<cr>", desc = "Iron Focus" },
+      { "<leader>rh", "<cmd>IronHide<cr>", desc = "Iron Hide" },
+    },
+    config = function()
+      local iron = require("iron.core")
+      local view = require("iron.view")
+      local executable = function(exe)
+        return vim.api.nvim_call_function("executable", { exe }) == 1
+      end
+
+      iron.setup({
+        config = {
+          -- Whether a repl should be discarded or not
+          scratch_repl = true,
+          -- Your repl definitions come here
+          repl_definition = {
+            python = {
+              command = { executable("bpython") and "bpython" or "python" },
+            },
+            sh = {
+              -- Can be a table or a function that
+              -- returns a table (see below)
+              command = { "zsh" },
+            },
+          },
+          -- How the repl window will be displayed
+          -- See below for more information
+          -- repl_open_cmd = require("iron.view").bottom(40),
+          repl_open_cmd = view.split.vertical.botright(100),
+        },
+        -- Iron doesn't set keymaps by default anymore.
+        -- You can set them here or manually add keymaps to the functions in iron.core
+        keymaps = {
+          send_motion = "<space>sc",
+          visual_send = "<space>sc",
+          send_file = "<space>sf",
+          send_line = "<space>sl",
+          send_until_cursor = "<space>su",
+          send_mark = "<space>sm",
+          mark_motion = "<space>mc",
+          mark_visual = "<space>mc",
+          remove_mark = "<space>md",
+          cr = "<space>s<cr>",
+          interrupt = "<space>s<space>",
+          exit = "<space>sq",
+          clear = "<space>cl",
+        },
+        -- If the highlight is on, you can change how it looks
+        -- For the available options, check nvim_set_hl
+        highlight = {
+          italic = true,
+        },
+        ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
+      })
+    end,
+  },
 }

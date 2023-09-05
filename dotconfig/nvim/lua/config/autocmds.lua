3e8bfd4c43076e349de367c08f1be82cb1e9e531
Yang Yang <kiooss@gmail.com>
2023-09-05 16:20:48 +0800

    Update.

--- dotconfig/nvim/lua/config/autocmds.lua
+++ dotconfig/nvim/lua/config/autocmds.lua
@@ -184,3 +184,11 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     vim.go.backupext = backup
   end,
 })
+
+vim.api.nvim_create_autocmd("WinLeave", {
+  callback = function()
+    if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
+      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "i", false)
+    end
+  end,
+})

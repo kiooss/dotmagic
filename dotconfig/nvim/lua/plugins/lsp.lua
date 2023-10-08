b602467cf2ae32987a0bd01979a07632a83b7e2d
Yang Yang <kiooss@gmail.com>
2023-10-08 09:37:37 +0800

    chore: update.

--- dotconfig/nvim/lua/plugins/lsp.lua
+++ dotconfig/nvim/lua/plugins/lsp.lua
@@ -64,7 +64,7 @@ return {
 
   -- null-ls
   {
-    "jose-elias-alvarez/null-ls.nvim",
+    "nvimtools/none-ls.nvim",
     opts = function(_, opts)
       local nls = require("null-ls")
       vim.list_extend(opts, {
--- dotconfig/nvim/lua/plugins/treesitter.lua
+++ dotconfig/nvim/lua/plugins/treesitter.lua
@@ -14,7 +14,7 @@ return {
 
   {
     "nvim-treesitter/nvim-treesitter-context",
-    event = "BufReadPre",
+    event = "User LazyFile",
     enabled = true,
     opts = { mode = "cursor" },
   },
@@ -73,24 +73,24 @@ return {
         lint_events = { "BufWrite", "CursorHold" },
       },
       highlight = { enable = true },
-      playground = {
-        enable = true,
-        disable = {},
-        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-        persist_queries = true, -- Whether the query persists across vim sessions
-        keybindings = {
-          toggle_query_editor = "o",
-          toggle_hl_groups = "i",
-          toggle_injected_languages = "t",
-          toggle_anonymous_nodes = "a",
-          toggle_language_display = "I",
-          focus_language = "f",
-          unfocus_language = "F",
-          update = "R",
-          goto_node = "<cr>",
-          show_help = "?",
-        },
-      },
+      -- playground = {
+      --   enable = true,
+      --   disable = {},
+      --   updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+      --   persist_queries = true, -- Whether the query persists across vim sessions
+      --   keybindings = {
+      --     toggle_query_editor = "o",
+      --     toggle_hl_groups = "i",
+      --     toggle_injected_languages = "t",
+      --     toggle_anonymous_nodes = "a",
+      --     toggle_language_display = "I",
+      --     focus_language = "f",
+      --     unfocus_language = "F",
+      --     update = "R",
+      --     goto_node = "<cr>",
+      --     show_help = "?",
+      --   },
+      -- },
     },
   },
   {

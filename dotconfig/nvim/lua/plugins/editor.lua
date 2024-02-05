953bd01fdc20ae760ae67821a23e386911fb607a
Michael Yang <kiooss@gmail.com>
2024-02-05 11:25:17 +0800

    Update.

--- dotconfig/nvim/lua/plugins/editor.lua
+++ dotconfig/nvim/lua/plugins/editor.lua
@@ -46,4 +46,16 @@ return {
       },
     },
   },
+
+  {
+    "rasulomaroff/reactive.nvim",
+    event = "VeryLazy",
+    opts = {
+      builtin = {
+        cursorline = true,
+        cursor = true,
+        modemsg = true,
+      },
+    },
+  },
 }
--- hammerspoon/audio_device.lua
+++ hammerspoon/audio_device.lua
@@ -35,12 +35,34 @@ function obj:init()
       hs.alert.show("Audio output device is now: " .. name)
     end
   end)
+
+  hs.hotkey.bind({}, "f13", "Switch audio output", function()
+    obj:switch()
+  end)
 end
 
 function obj:show()
   self.audioChooser:choices(obj.getAudioChoices())
   self.audioChooser:placeholderText("Select audio output device")
   self.audioChooser:show()
+end
+
+function obj:switch()
+  local uid_1 = "410C85C2-0000-0000-0E20-0104B53C2278"
+  local uid_2 = "BuiltInHeadphoneOutputDevice"
+  local next_uid
+  util.d(hs.audiodevice.current())
+  local current = hs.audiodevice.current()["device"]
+  util.d(current:uid())
+  if current:uid() == uid_1 then
+    next_uid = uid_2
+  else
+    next_uid = uid_1
+  end
+
+  if hs.audiodevice.findDeviceByUID(next_uid):setDefaultOutputDevice() then
+    hs.alert.show("Audio output switched to: " .. hs.audiodevice.current()["device"]:name())
+  end
 end
 
 return obj

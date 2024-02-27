00d03b17518e76a0acddd3adcb53175e8a05ac7e
Michael Yang <kiooss@gmail.com>
2024-02-27 15:58:25 +0800

    feat(nvim): rubocopA.

--- dotconfig/nvim/lua/plugins/formatting.lua
+++ dotconfig/nvim/lua/plugins/formatting.lua
@@ -2,6 +2,16 @@ return {
   {
     "stevearc/conform.nvim",
     optional = true,
+    keys = {
+      {
+        "<leader>aF",
+        function()
+          require("conform").format({ formatters = { "rubocopA" } })
+        end,
+        mode = { "n", "v" },
+        desc = "Rubocop auto-correct-all",
+      },
+    },
     opts = {
       formatters_by_ft = {
         ["markdown"] = { { "prettierd", "prettier" } },
@@ -23,10 +33,22 @@ return {
           end,
         },
         rubocop = {
-          prepend_args = { "--server" },
           condition = function(self, ctx)
             return vim.fs.find({ ".rubocop.yml" }, { path = ctx.filename, upward = true })[1]
           end,
+        },
+        rubocopA = {
+          command = "rubocop",
+          args = {
+            "--server",
+            "-A",
+            "-f",
+            "quiet",
+            "--stderr",
+            "--stdin",
+            "$FILENAME",
+          },
+          exit_codes = { 0, 1 },
         },
         htmlbeautifier = {
           condition = function(self, ctx)
